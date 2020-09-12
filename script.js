1cff4871d659eb539c52fdf5fa73eb7bd773d931
Corvette653 <56157995+Corvette653@users.noreply.github.com>
2020-09-12 12:55:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.querySelector("div:nth-child(1) input").addEventListener("change", licz
 document.querySelector("div:nth-child(2) input").addEventListener("change", licz, false);
 
 function licz() {
-	var liczba_do_zmiany = document.querySelector("div:nth-child(3) input").value;
-	var system = document.querySelector("div:nth-child(4) input").value;
+	var liczba_do_zmiany = document.querySelector("div:nth-child(1) input").value;
+	var system = document.querySelector("div:nth-child(2) input").value;
 	var system_int = parseInt(system);
 	var wynik = parseInt(liczba_do_zmiany[0]);
 
@@ -21,26 +21,3 @@ function licz() {
 
 	document.querySelector("input:read-only").value = wynik;
 }
-
-/*
-    QString tekst = ui->lineEdit_2->text();
-    int system = ui->spinBox->value();
-    unsigned long long wynik;
-    for (int i = 0; i < tekst.length(); i++) {
-        if (tekst[i].digitValue() == -1 || tekst[i].digitValue() >= system) {
-            QMessageBox wiadomosc;
-            wiadomosc.setText(tekst + " - to nie jest liczba w systemie (" + QString::number(system) + ")!!!");
-            wiadomosc.exec();
-            return;
-        }
-    }
-
-    wynik = tekst[0].digitValue();
-
-    for (int i = 1; i < tekst.length(); i++) {
-        wynik *= system;
-        wynik += tekst[i].digitValue();
-    }
-
-    tekst = QString::number(wynik);
-*/
